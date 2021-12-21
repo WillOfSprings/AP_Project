ec7150ed032f1c603017586a89bdf441e222d13b
Pratyush Kumar <pratyush20454@iiitd.ac.in>
2021-12-21 19:37:35 +0530

    players moving correctly

--- src/main/java/ap/ap_project/tempPlayer.java
+++ src/main/java/ap/ap_project/tempPlayer.java
@@ -10,26 +10,55 @@ import java.util.Map;
 // y distance = 65.3
 // 1 at 33x653.7
 
+class coords{
+    double x;
+    double y;
+    int multiplier;
+    coords(double x, double y, int multiplier){
+        this.x = x;
+        this.y = y;
+        this.multiplier = multiplier;
+    }
+
+    public double getX(){
+        return x;
+    }
+
+    public double getY(){
+        return y;
+    }
+
+    public int getMultiplier(){
+        return multiplier;
+    }
+}
+
 
 public class tempPlayer {
     double Xdistance = 45.8f;
     double Ydistance = 65.3f;
 
-    static HashMap<Integer, Integer> snakes;
-    static HashMap<Integer, Integer> ladders;
+    static HashMap<Integer, coords> snakes;
+    static HashMap<Integer, coords> ladders;
     ImageView piece;
     int position;
     double lX;
     double lY;
 
     static {
-        snakes = new HashMap<Integer,Integer>();
-        snakes.put(24,5); snakes.put(56,25); snakes.put(43,22); snakes.put(60,42); snakes.put(69,48);
-        snakes.put(86,53);snakes.put(98,58); snakes.put(96,84); snakes.put(94,73);snakes.put(90,72);
+        snakes = new HashMap<Integer,coords>();
+        snakes.put(24,new coords(226.1, 665.35, 1));snakes.put(56,new coords(226.1, 534.75, 1));
+        snakes.put(43,new coords(88.7, 534.75, 1));snakes.put(60,new coords(226.1, 404.15, 1));
+        snakes.put(69,new coords(363.5, 404.15, 1));snakes.put(86,new coords(363.5, 338.85, -1));
+        snakes.put(98,new coords(134.5, 338.85, -1));snakes.put(96,new coords(180.3, 142.95, 1));
+        snakes.put(94,new coords(363.5, 208.25, -1));snakes.put(90,new coords(409.3, 208.25, -1));
 
-        ladders  = new HashMap<Integer,Integer>();
-        ladders.put(3,21); ladders.put(29,33); ladders.put(16,26); ladders.put(8,46); ladders.put(37,65); ladders.put(64,77);
-        ladders.put(50,70); ladders.put(61,82); ladders.put(76,95); ladders.put(89,91);
+        ladders  = new HashMap<Integer,coords>();
+        ladders.put(3,new coords(42.9, 534.75, 1)); ladders.put(29,new coords(363.5, 469.45, -1));
+        ladders.put(16,new coords(271.9, 534.75, 1)); ladders.put(8,new coords(271.9, 404.15, 1));
+        ladders.put(37,new coords(226.1, 273.55, 1)); ladders.put(64,new coords(180.3, 208.25, -1));
+        ladders.put(50,new coords(455.1, 273.55, 1)); ladders.put(61,new coords(88.7, 142.95, 1));
+        ladders.put(76,new coords(271.9, 77.65, -1)); ladders.put(89,new coords(455.1, 455.1, -1));
 
     }
 
@@ -90,13 +119,13 @@ public class tempPlayer {
 
         position += dc;
         if (snakes.containsKey(position)) {
-            int d = position - (int) snakes.get(position);
-            move(d);
+            this.setCoords(snakes.get(position).getX(), snakes.get(position).getY());
+            Xdistance *= snakes.get(position).getMultiplier();
         }
 
         if (ladders.containsKey(position)) {
-            int d = (int) ladders.get(position) - position;
-            move(d);
+            this.setCoords(ladders.get(position).getX(), ladders.get(position).getY());
+            Xdistance *= ladders.get(position).getMultiplier();
         }
         if (position == 100) {
             System.out.println(" Winner ");
